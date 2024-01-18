6f0ad65b4ab7b86185bedebdac6c462a8b1d1a4b
Jai <jai07rajput@gmail.com>
2024-01-18 18:11:30 +0530

    Added Auth

---
+++ boult/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  boult
+//
+//  Created by Jai  on 18/01/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ boult/Core/Authentication/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  boult
+//
+//  Created by Jai  on 18/01/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
