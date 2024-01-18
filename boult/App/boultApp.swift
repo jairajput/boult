911c447cda93bbdec18e436b407633a2cb7209cf
Jai <jai07rajput@gmail.com>
2024-01-18 18:07:22 +0530

    AddedFirease-KingFisher

---
+++ boult/App/boultApp.swift
@@ -0,0 +1,31 @@
+//
+//  boultApp.swift
+//  boult
+//
+//  Created by Jai  on 18/01/24.
+//
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+
+@main
+struct boultApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
