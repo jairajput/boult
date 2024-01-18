86e3b1b48e2bb969f15c709a358ac1a5c5593868
Jai <jai07rajput@gmail.com>
2024-01-18 18:16:10 +0530

    Added-LoginScreen

--- boult/Core/Authentication/View/RegistrationView.swift
+++ boult/Core/Authentication/View/RegistrationView.swift
@@ -5,14 +5,3 @@
 //  Created by Jai  on 18/01/24.
 //
 
-import SwiftUI
-
-struct RegistrationView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    RegistrationView()
-}
