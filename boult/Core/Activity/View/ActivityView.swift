3942baf3821c4ed49b8abe4aff77136a9a396a6f
Jai <jai07rajput@gmail.com>
2024-01-18 18:08:52 +0530

    Activity

---
+++ boult/Core/Activity/View/ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  boult
+//
+//  Created by Jai  on 18/01/24.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text("Activity")
+    }
+}
+
+#Preview {
+    ActivityView()
+}
