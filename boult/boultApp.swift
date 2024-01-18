c1c86521d9dc0e8b7be90c62d92231c59faa47b8
Jai <jai07rajput@gmail.com>
2024-01-18 16:46:23 +0530

    first commit

---
+++ boult/boultApp.swift
@@ -0,0 +1,17 @@
+//
+//  boultApp.swift
+//  boult
+//
+//  Created by Jai  on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct boultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
