73f19cad4453e6b55c43fa59d286b6c0fcbb5299
Jai <jai07rajput@gmail.com>
2024-01-19 10:32:03 +0530

    ContentView-VM

---
+++ boult/Core/Root/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  boult
+//
+//  Created by Jai  on 18/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel()
+    
+    var body: some View {
+        Group {
+            if viewModel.userSession != nil {
+                BoultsTabView()
+            } else {
+                LoginView()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
