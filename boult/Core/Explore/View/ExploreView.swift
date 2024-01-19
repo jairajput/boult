7f548d54e9cf5db4946389f5b97f1f99a22ff50a
Jai <jai07rajput@gmail.com>
2024-01-19 09:48:56 +0530

    ExploreView

---
+++ boult/Core/Explore/View/ExploreView.swift
@@ -0,0 +1,41 @@
+//
+//  ExploreView.swift
+//  boult
+//
+//  Created by Jai  on 19/01/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    @State private var searchText = ""
+    @StateObject var viewModel = ExploreViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.users) { user in
+                        NavigationLink(value: user) {
+                            VStack {
+                                UserCell(user: user)
+                                
+                                Divider()
+                            }
+                            .padding(.vertical, 4)
+                        }
+                    }
+                }
+            }
+            .navigationDestination(for: User.self, destination: { user in
+                ProfileView(user: user)
+            })
+            .navigationTitle("Search")
+            .searchable(text: $searchText, prompt: "Search")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ boult/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ExploreViewModel.swift
+//  boult
+//
+//  Created by Jai  on 19/01/24.
+//
+
+import Foundation
