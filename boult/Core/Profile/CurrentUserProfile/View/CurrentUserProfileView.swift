f3305d0af13a0cc376c8a7194e1dbc237f6e10ea
Jai <jai07rajput@gmail.com>
2024-01-19 10:18:42 +0530

    Added-UserProfileView

---
+++ boult/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
@@ -0,0 +1,69 @@
+//
+//  CurrentUserProfileView.swift
+//  boult
+//
+//  Created by Jai  on 19/01/24.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    @StateObject var viewModel = CurrentUserProfileViewModel()
+    @State private var showEditProfile = false
+    
+    private var currentUser: User? {
+        return viewModel.currentUser
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                // bio and stats
+                VStack(spacing: 20) {
+                        ProfileHeaderView(user: currentUser)
+                    
+                    Button {
+                        showEditProfile.toggle()
+                    } label: {
+                    Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                            .frame(width: 352, height: 32)
+                            .background(.white)
+                            .cornerRadius(8)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color(.systemGray4), lineWidth: 1)
+                            }
+                    }
+                    
+                    // user content list view
+                    if let user = currentUser {
+                        UserContentListView(user: user)
+                    }
+                }
+            }
+            .sheet(isPresented: $showEditProfile, content: {
+                if let user = currentUser {
+                    EditProfileView(user: user)
+                }
+            })
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        AuthService.shared.signOut()
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+
+#Preview {
+    CurrentUserProfileView()
+}
